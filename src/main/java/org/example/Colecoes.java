eb901bf91c1930bce6cb93a8099b0d7a5693ce26
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-11-02 08:04:23 -0400

    a

---
+++ src/main/java/org/example/Colecoes.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Colecoes {
+    public static void main(String[] args) {
+        Map<String, Integer> notas = new HashMap<>();
+        notas.put("Fulano", 10);
+        notas.put("Cicrano", 8);
+        notas.put("Deltrano", 7);
+
+        System.out.printf("A nota do Fulano é: %d\n", notas.get("Fulano"));
+
+        for (Map.Entry<String, Integer> entrada : notas.entrySet()){
+            String chave = entrada.getKey();
+            Integer valor = entrada.getValue();
+
+            System.out.printf("A chave é %s e o valor é %d\n", chave, valor);
+        }
+    }
+}
